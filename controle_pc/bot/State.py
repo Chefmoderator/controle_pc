eef848f08d33286a31495ba6598af011fb8fa665
Chefmoderator <148126189+Chefmoderator@users.noreply.github.com>
2026-01-23 09:28:05 +0200

    Delete controle_pc directory

--- controle_pc/bot/State.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-class BotState(StatesGroup):
-    neutral = State()
-    confirmation_of_fsm_stop = State()
-    waiting_for_asking_for_change_volume = State()
-    processed_for_change_volume = State()
-    processed_turn_on_the_sound = State()
-    processed_turn_off_the_sound = State()
